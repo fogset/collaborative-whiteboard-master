5d4e081b055cff509098f2ded1f619ae6a36e4da
fogset <zhangth1990@hotmail.com>
2021-09-30 17:32:43 -0700

    delete unused file and add private room and join room feature

--- server/server.js
+++ server/server.js
@@ -2,16 +2,23 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-io.on('connection', (socket)=> {
-      console.log('User Online');
+io.on('connection', (socket) => {
+      console.log(`User Connected: ${socket.id}`);
 
-      socket.on('canvas-data', (data)=> {
+      socket.on("join_room", (data) => {
+            socket.join(data);
+            console.log(`User with ID: ${socket.id} joined room: ${data}`);
+      });
+
+      socket.on('canvas-data', (data) => {
             socket.broadcast.emit('canvas-data', data);
-            
       })
+      socket.on("disconnect", () => {
+            console.log("User Disconnected", socket.id);
+      });
 })
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
 http.listen(server_port, () => {
-    console.log("Started on : "+ server_port);
+      console.log("Started on : " + server_port);
 })
--- ui/whiteboard-collab/src/App.js
+++ ui/whiteboard-collab/src/App.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import io from 'socket.io-client';
 import Container from './components/container/Container';
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  let socket;
+
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket = io.connect("http://localhost:5000");
+      socket.emit("join_room", room);
+
+      //setShowChat(true);
+    }
+  };
+
   return (
-    <Container/>
+    <div className="App">
+      <h3>Join A Chat</h3>
+      <input
+        type="text"
+        placeholder="John..."
+        onChange={(event) => { setUsername(event.target.value); }}
+      />
+      <input
+        type="text"
+        placeholder="Room ID..."
+        onChange={(event) => { setRoom(event.target.value); }}
+      />
+      <button onClick={joinRoom}>Join A Room</button>
+
+    </div>
+
+
+
+
+    //<Container />
   );
 }
 
